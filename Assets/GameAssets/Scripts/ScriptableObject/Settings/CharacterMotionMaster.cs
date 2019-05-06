83477b509af2c5232b855ba164d93a7168de950e
ishidafuu <ishidafuu@gmail.com>
2019-05-06 14:00:46 +0900

    キャラモーションマスタークラス追加

---
+++ Assets/GameAssets/Scripts/ScriptableObject/Settings/CharacterMotionMaster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NKPB
+{
+    [Serializable]
+    public class CharacterMotionMaster : ScriptableObject
+    {
+        public List<CharacterMotionData> motionDatas = new List<CharacterMotionData>();
+    }
+}
