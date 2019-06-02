698bbb9f65cb4d02d0c027312681c556e2d308e6
ishidafuu <ishidafuu@gmail.com>
2019-06-03 00:51:37 +0900

    [update]闘気メーター処理一時退避（後ほど削除）

--- Assets/GameAssets/Scripts/ECS/Systems/Update/ToukiMeterInputJobSystem.cs
+++ Assets/GameAssets/Scripts/ECS/Systems/Update/ToukiMeterInputJobSystem.cs
@@ -1,96 +1,96 @@
-﻿using Unity.Burst;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Jobs;
-using Unity.Mathematics;
-using Unity.Transforms;
-using UnityEngine;
+﻿// using Unity.Burst;
+// using Unity.Collections;
+// using Unity.Entities;
+// using Unity.Jobs;
+// using Unity.Mathematics;
+// using Unity.Transforms;
+// using UnityEngine;
 
-namespace NKPB
-{
-    [UpdateInGroup(typeof(UpdateGroup))]
-    public class ToukiMeterInputJobSystem : JobComponentSystem
-    {
-        ComponentGroup m_group;
+// namespace NKPB
+// {
+//     [UpdateInGroup(typeof(UpdateGroup))]
+//     public class ToukiMeterInputJobSystem : JobComponentSystem
+//     {
+//         ComponentGroup m_group;
 
-        protected override void OnCreateManager()
-        {
-            m_group = GetComponentGroup(
-                ComponentType.ReadOnly<PadScan>(),
-                ComponentType.Create<ToukiMeter>()
-            );
+//         protected override void OnCreateManager()
+//         {
+//             m_group = GetComponentGroup(
+//                 ComponentType.ReadOnly<PadScan>(),
+//                 ComponentType.Create<ToukiMeter>()
+//             );
 
-        }
+//         }
 
-        protected override JobHandle OnUpdate(JobHandle inputDeps)
-        {
-            m_group.AddDependency(inputDeps);
+//         protected override JobHandle OnUpdate(JobHandle inputDeps)
+//         {
+//             m_group.AddDependency(inputDeps);
 
-            var PadScans = m_group.ToComponentDataArray<PadScan>(Allocator.TempJob, out JobHandle handle1);
-            var toukiMeters = m_group.ToComponentDataArray<ToukiMeter>(Allocator.TempJob, out JobHandle handle2);
-            inputDeps = JobHandle.CombineDependencies(handle1, handle2);
+//             var PadScans = m_group.ToComponentDataArray<PadScan>(Allocator.TempJob, out JobHandle handle1);
+//             var toukiMeters = m_group.ToComponentDataArray<ToukiMeter>(Allocator.TempJob, out JobHandle handle2);
+//             inputDeps = JobHandle.CombineDependencies(handle1, handle2);
 
-            var job = new InputToToukiJob()
-            {
-                PadScans = PadScans,
-                toukiMeters = toukiMeters,
-            };
-            inputDeps = job.Schedule(inputDeps);
+//             var job = new InputToToukiJob()
+//             {
+//                 PadScans = PadScans,
+//                 toukiMeters = toukiMeters,
+//             };
+//             inputDeps = job.Schedule(inputDeps);
 
-            m_group.AddDependency(inputDeps);
-            m_group.CopyFromComponentDataArray(toukiMeters, out JobHandle handle3);
-            // inputDeps.Complete();
+//             m_group.AddDependency(inputDeps);
+//             m_group.CopyFromComponentDataArray(toukiMeters, out JobHandle handle3);
+//             // inputDeps.Complete();
 
-            inputDeps = new ReleaseJob
-            {
-                toukiMeters = toukiMeters
-            }.Schedule(handle3);
+//             inputDeps = new ReleaseJob
+//             {
+//                 toukiMeters = toukiMeters
+//             }.Schedule(handle3);
 
-            return inputDeps;
-        }
+//             return inputDeps;
+//         }
 
-        struct ReleaseJob : IJob
-        {
-            [DeallocateOnJobCompletion]
-            public NativeArray<ToukiMeter> toukiMeters;
+//         struct ReleaseJob : IJob
+//         {
+//             [DeallocateOnJobCompletion]
+//             public NativeArray<ToukiMeter> toukiMeters;
 
-            public void Execute() {}
-        }
+//             public void Execute() {}
+//         }
 
-        [BurstCompileAttribute]
-        struct InputToToukiJob : IJob
-        {
-            [ReadOnly]
-            [DeallocateOnJobCompletion]
-            public NativeArray<PadScan> PadScans;
+//         [BurstCompileAttribute]
+//         struct InputToToukiJob : IJob
+//         {
+//             [ReadOnly]
+//             [DeallocateOnJobCompletion]
+//             public NativeArray<PadScan> PadScans;
 
-            public NativeArray<ToukiMeter> toukiMeters;
+//             public NativeArray<ToukiMeter> toukiMeters;
 
-            public void Execute()
-            {
-                for (int i = 0; i < PadScans.Length; i++)
-                {
-                    var toukiMeter = toukiMeters[i];
-                    var a = true;
-                    var b = true;
-                    var c = true;
-                    var v = a || (b && c);
-                    if (toukiMeter.state != EnumToukiMaterState.Active)
-                    {
-                        break;
-                    }
+//             public void Execute()
+//             {
+//                 for (int i = 0; i < PadScans.Length; i++)
+//                 {
+//                     var toukiMeter = toukiMeters[i];
+//                     var a = true;
+//                     var b = true;
+//                     var c = true;
+//                     var v = a || (b && c);
+//                     if (toukiMeter.state != EnumToukiMaterState.Active)
+//                     {
+//                         break;
+//                     }
 
-                    if (toukiMeter.muki != PadScans[i].GetPressCross())
-                    {
-                        toukiMeter.muki = PadScans[i].GetPressCross();
-                        toukiMeter.value = 0;
-                    }
+//                     if (toukiMeter.muki != PadScans[i].GetPressCross())
+//                     {
+//                         toukiMeter.muki = PadScans[i].GetPressCross();
+//                         toukiMeter.value = 0;
+//                     }
 
-                    toukiMeters[i] = toukiMeter;
-                }
-            }
+//                     toukiMeters[i] = toukiMeter;
+//                 }
+//             }
 
-        }
+//         }
 
-    }
-}
+//     }
+// }
