04dccd763bf71a395b4a8b58134714e72d17f8a1
ishidafuu <ishidafuu@gmail.com>
2019-06-03 00:52:15 +0900

    [update]パス情報など別ファイルに持つように修正

---
+++ Assets/GameAssets/Scripts/ECS/PathSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NKPB
+{
+    public static class PathSettings
+    {
+        public static readonly string DefaultShader = "Sprites/Default";
+        public static readonly string CharaSprite = "Sprites/nm2body";
+
+    }
+}
--- Assets/GameAssets/Scripts/ECS/Shared.cs
+++ Assets/GameAssets/Scripts/ECS/Shared.cs
@@ -11,34 +11,34 @@ using UnityEngine.SceneManagement;
 
 namespace NKPB
 {
-	public static class Shared
-	{
-		// SharedComponentData
-		public static MeshMatList charaMeshMat;
-		public static AniScriptSheet aniScriptSheet;
-		public static AniBasePos aniBasePos;
-		public static MeshMatList bgFrameMeshMat;
-		// public static MeshMatList meterMeshMat;
+    public static class Shared
+    {
+        // SharedComponentData
+        public static MeshMatList charaMeshMat;
+        // public static AniScriptSheet aniScriptSheet;
+        // public static AniBasePos aniBasePos;
+        // public static MeshMatList bgFrameMeshMat;
+        // public static MeshMatList meterMeshMat;
 
-		static readonly string ShaderBg = "Sprites/DefaultSprite";
+        // static readonly string ShaderBg = "Sprites/DefaultSprite";
 
-		// SharedComponentDataの読み込み
-		public static void ReadySharedComponentData()
-		{
-			bgFrameMeshMat = new MeshMatList("yyhs/bg/bg", ShaderBg);
-			// meterMeshMat = new MeshMatList("yyhs/bg/meter", ShaderBg);
+        // SharedComponentDataの読み込み
+        public static void ReadySharedComponentData()
+        {
+            charaMeshMat = new MeshMatList(PathSettings.CharaSprite, PathSettings.DefaultShader);
+            // meterMeshMat = new MeshMatList("yyhs/bg/meter", ShaderBg);
 
-			// // スクリプタブルオブジェクトの読み込み
-			// aniScriptSheet = new AniScriptSheet();
-			// if (Resources.FindObjectsOfTypeAll<AniScriptSheetObject>().Length == 0)
-			// 	Debug.LogError("aniScriptSheet 0");
-			// aniScriptSheet.scripts = (Resources.FindObjectsOfTypeAll<AniScriptSheetObject>().First()as AniScriptSheetObject).scripts;
+            // // スクリプタブルオブジェクトの読み込み
+            // aniScriptSheet = new AniScriptSheet();
+            // if (Resources.FindObjectsOfTypeAll<AniScriptSheetObject>().Length == 0)
+            // 	Debug.LogError("aniScriptSheet 0");
+            // aniScriptSheet.scripts = (Resources.FindObjectsOfTypeAll<AniScriptSheetObject>().First()as AniScriptSheetObject).scripts;
 
-			// aniBasePos = new AniBasePos();
-			// if (Resources.FindObjectsOfTypeAll<AniBasePosObject>().Length == 0)
-			// 	Debug.LogError("aniBasePos 0");
-			// aniBasePos = (Resources.FindObjectsOfTypeAll<AniBasePosObject>().First()as AniBasePosObject).aniBasePos;
-		}
+            // aniBasePos = new AniBasePos();
+            // if (Resources.FindObjectsOfTypeAll<AniBasePosObject>().Length == 0)
+            // 	Debug.LogError("aniBasePos 0");
+            // aniBasePos = (Resources.FindObjectsOfTypeAll<AniBasePosObject>().First()as AniBasePosObject).aniBasePos;
+        }
 
-	}
+    }
 }
