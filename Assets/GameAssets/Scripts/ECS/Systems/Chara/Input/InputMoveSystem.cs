9c3871479ad5098e3bfb8f10672093dc7f6432d6
ishidafuu <ishidafuu@gmail.com>
2019-06-09 10:16:49 +0900

    [update]移動システムをジョブシステムに変更

--- Assets/GameAssets/Scripts/ECS/Systems/Chara/Input/InputMoveSystem.cs
+++ Assets/GameAssets/Scripts/ECS/Systems/Chara/Input/InputMoveSystem.cs
@@ -14,247 +14,194 @@ namespace NKPB
     /// <summary>
     /// 入力による向き変化システム
     /// </summary>
-    public class InputMoveSystem : ComponentSystem
+    public class InputMoveSystem : JobComponentSystem
     {
-        ComponentGroup group;
+        ComponentGroup m_group;
+        ComponentDataArray<CharaMove> m_charaMoves;
+        ComponentDataArray<CharaDash> m_charaDashs;
+        ComponentDataArray<CharaFlag> m_charaFlags;
+        ComponentDataArray<PadScan> m_padScans;
 
         protected override void OnCreateManager()
         {
-            group = GetComponentGroup(
+            m_group = GetComponentGroup(
                 ComponentType.Create<CharaMove>(),
                 ComponentType.ReadOnly<CharaDash>(),
                 ComponentType.ReadOnly<CharaFlag>(),
                 ComponentType.ReadOnly<PadScan>());
         }
 
-        ComponentDataArray<CharaMove> m_charaMoves;
-        ComponentDataArray<CharaDash> m_charaDashs;
-        ComponentDataArray<CharaFlag> m_charaFlags;
-        ComponentDataArray<PadScan> padScans;
-
-        protected override void OnUpdate()
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
-            m_charaMoves = group.GetComponentDataArray<CharaMove>();
-            m_charaDashs = group.GetComponentDataArray<CharaDash>();
-            m_charaFlags = group.GetComponentDataArray<CharaFlag>();
-            padScans = group.GetComponentDataArray<PadScan>();
-
-            for (int i = 0; i < m_charaFlags.Length; i++)
+            var job = new MoveJob()
             {
-                var charaFlag = m_charaFlags[i];
+                m_charaMoves = m_group.GetComponentDataArray<CharaMove>(),
+                m_charaDashs = m_group.GetComponentDataArray<CharaDash>(),
+                m_charaFlags = m_group.GetComponentDataArray<CharaFlag>(),
+                m_padScans = m_group.GetComponentDataArray<PadScan>(),
+            };
 
-                if (charaFlag.moveFlag.IsFlag(FlagMove.Walk))
-                {
-                    Walk(i);
-                }
-
-                if (charaFlag.moveFlag.IsFlag(FlagMove.Friction))
-                {
-                    Friction(i);
-                }
-
-                if (charaFlag.moveFlag.IsFlag(FlagMove.Stop))
-                {
-                    Stop(i);
-                }
-
-                if (charaFlag.moveFlag.IsFlag(FlagMove.Dash))
-                {
-                    Dash(i);
-                }
-            }
+            inputDeps = job.Schedule(inputDeps);
+            inputDeps.Complete();
+            return inputDeps;
         }
 
-        void Friction(int i)
+        [BurstCompileAttribute]
+        struct MoveJob : IJob
         {
-            CharaMove charaMove = m_charaMoves[i];
-            UpdateFriction(ref charaMove, Define.Instance.Move.BrakeDelta);
-            m_charaMoves[i] = charaMove;
-        }
+            public ComponentDataArray<CharaMove> m_charaMoves;
+            [ReadOnly]
+            public ComponentDataArray<CharaDash> m_charaDashs;
+            [ReadOnly]
+            public ComponentDataArray<CharaFlag> m_charaFlags;
+            [ReadOnly]
+            public ComponentDataArray<PadScan> m_padScans;
 
-        // XZ方向減速
-        void UpdateFriction(ref CharaMove charaMove, int _brakeDelta)
-        {
-            if (charaMove.delta.x > 0)
+            public void Execute()
             {
-                charaMove.delta.x = Mathf.Min(0, charaMove.delta.x - _brakeDelta);
-            }
-            else if (charaMove.delta.x < 0)
-            {
-                charaMove.delta.x = Mathf.Max(0, charaMove.delta.x + _brakeDelta);
-            }
 
-            if (charaMove.delta.z > 0)
-            {
-                charaMove.delta.z = Mathf.Min(0, charaMove.delta.z - _brakeDelta);
-            }
-            else if (charaMove.delta.x < 0)
-            {
-                charaMove.delta.z = Mathf.Max(0, charaMove.delta.z + _brakeDelta);
-            }
-        }
-
-        void Stop(int i)
-        {
-            CharaMove charaMove = m_charaMoves[i];
-            ClearDelta(ref charaMove);
-            m_charaMoves[i] = charaMove;
-        }
-
-        //XZ方向停止
-        void ClearDelta(ref CharaMove charaMove)
-        {
-            charaMove.delta.x = 0;
-            charaMove.delta.z = 0;
-        }
-
-        void Walk(int i)
-        {
-            // Debug.Log("Walk");
-            CharaMove charaMove = m_charaMoves[i];
-            SetDelta(ref charaMove, Define.Instance.Move.WalkSpeed, InputToMoveMuki(i));
-            m_charaMoves[i] = charaMove;
-        }
-
-        void SetDelta(ref CharaMove charaMove, int _delta, EnumMoveMuki _moveMuki)
-        {
-            charaMove.delta = DeltaToVector3Int(_delta, _moveMuki);
-        }
-
-        Vector3Int DeltaToVector3Int(int _delta, EnumMoveMuki _moveMuki)
-        {
-            Vector3 res = Vector3.zero;
-            const float NANAME45 = 0.7f;
-            const float NANAME30 = 0.5f;
-            const float NANAME30L = 0.87f;
-            switch (_moveMuki)
-            {
-                case EnumMoveMuki.Left:
-                    res = new Vector3(-1, 0, 0);
-                    break;
-                case EnumMoveMuki.LeftLeftDown:
-                    res = new Vector3(-NANAME30L, 0, -NANAME30);
-                    break;
-                case EnumMoveMuki.LeftDown:
-                    res = new Vector3(-NANAME45, 0, -NANAME45);
-                    break;
-                case EnumMoveMuki.LeftLeftUp:
-                    res = new Vector3(-NANAME30L, 0, +NANAME30);
-                    break;
-                case EnumMoveMuki.LeftUp:
-                    res = new Vector3(-NANAME45, 0, +NANAME45);
-                    break;
-                case EnumMoveMuki.Right:
-                    res = new Vector3(1, 0, 0);
-                    break;
-                case EnumMoveMuki.RightRightDown:
-                    res = new Vector3(+NANAME30L, 0, -NANAME30);
-                    break;
-                case EnumMoveMuki.RightDown:
-                    res = new Vector3(+NANAME45, 0, -NANAME45);
-                    break;
-                case EnumMoveMuki.RightRightUp:
-                    res = new Vector3(+NANAME30L, 0, +NANAME30);
-                    break;
-                case EnumMoveMuki.RightUp:
-                    res = new Vector3(+NANAME45, 0, +NANAME45);
-                    break;
-                case EnumMoveMuki.Up:
-                    res = new Vector3(0, 0, 1);
-                    break;
-                case EnumMoveMuki.Down:
-                    res = new Vector3(0, 0, -1);
-                    break;
-            }
-            res *= _delta;
-            return new Vector3Int((int)res.x, 0, (int)res.z);
-        }
-
-        EnumMoveMuki InputToMoveMuki(int i)
-        {
-            var res = EnumMoveMuki.None;
-
-            if (padScans[i].crossLeft.isPress)
-            {
-                if (padScans[i].crossUp.isPress)
+                for (int i = 0; i < m_charaFlags.Length; i++)
                 {
-                    res = EnumMoveMuki.LeftUp;
-                }
-                else if (padScans[i].crossDown.isPress)
-                {
-                    res = EnumMoveMuki.LeftDown;
-                }
-                else
-                {
-                    res = EnumMoveMuki.Left;
-                }
-            }
-            else if (padScans[i].crossRight.isPress)
-            {
-                if (padScans[i].crossUp.isPress)
-                {
-                    res = EnumMoveMuki.RightUp;
-                }
-                else if (padScans[i].crossDown.isPress)
-                {
-                    res = EnumMoveMuki.RightDown;
-                }
-                else
-                {
-                    res = EnumMoveMuki.Right;
-                }
-            }
-            else
-            {
-                if (padScans[i].crossUp.isPress)
-                {
-                    res = EnumMoveMuki.Up;
-                }
-                else if (padScans[i].crossDown.isPress)
-                {
-                    res = EnumMoveMuki.Down;
-                }
-            }
+                    var charaFlag = m_charaFlags[i];
 
-            return res;
-        }
-
-        void Dash(int i)
-        {
-            // Debug.Log("Dash");
-            CharaMove charaMove = m_charaMoves[i];
-            SetDelta(ref charaMove, Define.Instance.Move.WalkSpeed, InputToMoveMukiDash(i));
-            m_charaMoves[i] = charaMove;
-        }
-
-        EnumMoveMuki InputToMoveMukiDash(int i)
-        {
-            var res = EnumMoveMuki.None;
-            var charaDash = m_charaDashs[i];
-            if (charaDash.dashMuki == EnumMuki.Left)
-            {
-                if (padScans[i].crossLeft.isPress)
-                {
-                    if (padScans[i].crossUp.isPress)
+                    if (charaFlag.moveFlag.IsFlag(FlagMove.Walk))
                     {
-                        res = EnumMoveMuki.LeftLeftUp;
+                        Walk(i);
                     }
-                    else if (padScans[i].crossDown.isPress)
+
+                    if (charaFlag.moveFlag.IsFlag(FlagMove.Friction))
                     {
-                        res = EnumMoveMuki.LeftLeftDown;
+                        Friction(i);
                     }
-                    else
+
+                    if (charaFlag.moveFlag.IsFlag(FlagMove.Stop))
                     {
-                        res = EnumMoveMuki.Left;
+                        Stop(i);
+                    }
+
+                    if (charaFlag.moveFlag.IsFlag(FlagMove.Dash))
+                    {
+                        Dash(i);
                     }
                 }
-                else
+            }
+
+            void Friction(int i)
+            {
+                CharaMove charaMove = m_charaMoves[i];
+                UpdateFriction(ref charaMove, Define.Instance.Move.BrakeDelta);
+                m_charaMoves[i] = charaMove;
+            }
+
+            // XZ方向減速
+            void UpdateFriction(ref CharaMove charaMove, int _brakeDelta)
+            {
+                if (charaMove.delta.x > 0)
                 {
-                    if (padScans[i].crossUp.isPress)
+                    charaMove.delta.x = Mathf.Min(0, charaMove.delta.x - _brakeDelta);
+                }
+                else if (charaMove.delta.x < 0)
+                {
+                    charaMove.delta.x = Mathf.Max(0, charaMove.delta.x + _brakeDelta);
+                }
+
+                if (charaMove.delta.z > 0)
+                {
+                    charaMove.delta.z = Mathf.Min(0, charaMove.delta.z - _brakeDelta);
+                }
+                else if (charaMove.delta.x < 0)
+                {
+                    charaMove.delta.z = Mathf.Max(0, charaMove.delta.z + _brakeDelta);
+                }
+            }
+
+            void Stop(int i)
+            {
+                CharaMove charaMove = m_charaMoves[i];
+                ClearDelta(ref charaMove);
+                m_charaMoves[i] = charaMove;
+            }
+
+            //XZ方向停止
+            void ClearDelta(ref CharaMove charaMove)
+            {
+                charaMove.delta.x = 0;
+                charaMove.delta.z = 0;
+            }
+
+            void Walk(int i)
+            {
+                // Debug.Log("Walk");
+                CharaMove charaMove = m_charaMoves[i];
+                SetDelta(ref charaMove, Define.Instance.Move.WalkSpeed, InputToMoveMuki(i));
+                m_charaMoves[i] = charaMove;
+            }
+
+            void SetDelta(ref CharaMove charaMove, int _delta, EnumMoveMuki _moveMuki)
+            {
+                charaMove.delta = DeltaToVector3Int(_delta, _moveMuki);
+            }
+
+            Vector3Int DeltaToVector3Int(int _delta, EnumMoveMuki _moveMuki)
+            {
+                Vector3 res = Vector3.zero;
+                const float NANAME45 = 0.7f;
+                const float NANAME30 = 0.5f;
+                const float NANAME30L = 0.87f;
+                switch (_moveMuki)
+                {
+                    case EnumMoveMuki.Left:
+                        res = new Vector3(-1, 0, 0);
+                        break;
+                    case EnumMoveMuki.LeftLeftDown:
+                        res = new Vector3(-NANAME30L, 0, -NANAME30);
+                        break;
+                    case EnumMoveMuki.LeftDown:
+                        res = new Vector3(-NANAME45, 0, -NANAME45);
+                        break;
+                    case EnumMoveMuki.LeftLeftUp:
+                        res = new Vector3(-NANAME30L, 0, +NANAME30);
+                        break;
+                    case EnumMoveMuki.LeftUp:
+                        res = new Vector3(-NANAME45, 0, +NANAME45);
+                        break;
+                    case EnumMoveMuki.Right:
+                        res = new Vector3(1, 0, 0);
+                        break;
+                    case EnumMoveMuki.RightRightDown:
+                        res = new Vector3(+NANAME30L, 0, -NANAME30);
+                        break;
+                    case EnumMoveMuki.RightDown:
+                        res = new Vector3(+NANAME45, 0, -NANAME45);
+                        break;
+                    case EnumMoveMuki.RightRightUp:
+                        res = new Vector3(+NANAME30L, 0, +NANAME30);
+                        break;
+                    case EnumMoveMuki.RightUp:
+                        res = new Vector3(+NANAME45, 0, +NANAME45);
+                        break;
+                    case EnumMoveMuki.Up:
+                        res = new Vector3(0, 0, 1);
+                        break;
+                    case EnumMoveMuki.Down:
+                        res = new Vector3(0, 0, -1);
+                        break;
+                }
+                res *= _delta;
+                return new Vector3Int((int)res.x, 0, (int)res.z);
+            }
+
+            EnumMoveMuki InputToMoveMuki(int i)
+            {
+                var res = EnumMoveMuki.None;
+
+                if (m_padScans[i].crossLeft.isPress)
+                {
+                    if (m_padScans[i].crossUp.isPress)
                     {
                         res = EnumMoveMuki.LeftUp;
                     }
-                    else if (padScans[i].crossDown.isPress)
+                    else if (m_padScans[i].crossDown.isPress)
                     {
                         res = EnumMoveMuki.LeftDown;
                     }
@@ -263,31 +210,13 @@ namespace NKPB
                         res = EnumMoveMuki.Left;
                     }
                 }
-            }
-            else if (charaDash.dashMuki == EnumMuki.Right)
-            {
-                if (padScans[i].crossRight.isPress)
+                else if (m_padScans[i].crossRight.isPress)
                 {
-                    if (padScans[i].crossUp.isPress)
-                    {
-                        res = EnumMoveMuki.RightRightUp;
-                    }
-                    else if (padScans[i].crossDown.isPress)
-                    {
-                        res = EnumMoveMuki.RightRightDown;
-                    }
-                    else
-                    {
-                        res = EnumMoveMuki.Right;
-                    }
-                }
-                else
-                {
-                    if (padScans[i].crossUp.isPress)
+                    if (m_padScans[i].crossUp.isPress)
                     {
                         res = EnumMoveMuki.RightUp;
                     }
-                    else if (padScans[i].crossDown.isPress)
+                    else if (m_padScans[i].crossDown.isPress)
                     {
                         res = EnumMoveMuki.RightDown;
                     }
@@ -296,8 +225,102 @@ namespace NKPB
                         res = EnumMoveMuki.Right;
                     }
                 }
+                else
+                {
+                    if (m_padScans[i].crossUp.isPress)
+                    {
+                        res = EnumMoveMuki.Up;
+                    }
+                    else if (m_padScans[i].crossDown.isPress)
+                    {
+                        res = EnumMoveMuki.Down;
+                    }
+                }
+
+                return res;
             }
-            return res;
+
+            void Dash(int i)
+            {
+                // Debug.Log("Dash");
+                CharaMove charaMove = m_charaMoves[i];
+                SetDelta(ref charaMove, Define.Instance.Move.WalkSpeed, InputToMoveMukiDash(i));
+                m_charaMoves[i] = charaMove;
+            }
+
+            EnumMoveMuki InputToMoveMukiDash(int i)
+            {
+                var res = EnumMoveMuki.None;
+                var charaDash = m_charaDashs[i];
+                if (charaDash.dashMuki == EnumMuki.Left)
+                {
+                    if (m_padScans[i].crossLeft.isPress)
+                    {
+                        if (m_padScans[i].crossUp.isPress)
+                        {
+                            res = EnumMoveMuki.LeftLeftUp;
+                        }
+                        else if (m_padScans[i].crossDown.isPress)
+                        {
+                            res = EnumMoveMuki.LeftLeftDown;
+                        }
+                        else
+                        {
+                            res = EnumMoveMuki.Left;
+                        }
+                    }
+                    else
+                    {
+                        if (m_padScans[i].crossUp.isPress)
+                        {
+                            res = EnumMoveMuki.LeftUp;
+                        }
+                        else if (m_padScans[i].crossDown.isPress)
+                        {
+                            res = EnumMoveMuki.LeftDown;
+                        }
+                        else
+                        {
+                            res = EnumMoveMuki.Left;
+                        }
+                    }
+                }
+                else if (charaDash.dashMuki == EnumMuki.Right)
+                {
+                    if (m_padScans[i].crossRight.isPress)
+                    {
+                        if (m_padScans[i].crossUp.isPress)
+                        {
+                            res = EnumMoveMuki.RightRightUp;
+                        }
+                        else if (m_padScans[i].crossDown.isPress)
+                        {
+                            res = EnumMoveMuki.RightRightDown;
+                        }
+                        else
+                        {
+                            res = EnumMoveMuki.Right;
+                        }
+                    }
+                    else
+                    {
+                        if (m_padScans[i].crossUp.isPress)
+                        {
+                            res = EnumMoveMuki.RightUp;
+                        }
+                        else if (m_padScans[i].crossDown.isPress)
+                        {
+                            res = EnumMoveMuki.RightDown;
+                        }
+                        else
+                        {
+                            res = EnumMoveMuki.Right;
+                        }
+                    }
+                }
+                return res;
+            }
         }
+
     }
 }
